766c263423e53f80f58958ffda8195b8d3426eda
Derrick <derrickbond89@gmail.com>
2020-09-19 15:11:31 -0600

    Added Night vision toggle for player
    = Increased camera move speed
    + Added Night-vision goggle overlay to UI
    + LightSettingsPlayer.cs: handles toggle for nightvision on/off

---
+++ Horror_Survival_Game/Assets/Scripts/Camera/LightSettingsPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class LightSettingsPlayer : MonoBehaviour
+{
+    [SerializeField] PostProcessVolume MyVolume;
+
+    //Standard Profile that lights the scene
+    [SerializeField] PostProcessProfile StandardProfile; 
+    //Profile used when player toggles the Night-vision goggles on
+    [SerializeField] PostProcessProfile NightVisionProfile;
+    //The GUI overlay used when NightVisionProfile is active
+    [SerializeField] GameObject NightVisionOverlay;
+    
+    //NightVisionActive true when toggled on, default false
+    private bool NightVisionActive = false;
+
+    private void Start()
+    {
+        NightVisionOverlay.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Toggles NightVision on/off by pressing 'N'
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            ToggleNightVision();
+        }
+    }
+
+    /* 
+     * Toggles Night-vision mode on/off
+     * Default: NightVisionActive is false
+     */
+    void ToggleNightVision()
+    {
+        //if NightVision not currently active
+        if(!NightVisionActive)
+        {
+            MyVolume.profile = NightVisionProfile;
+            NightVisionActive = true;
+            NightVisionOverlay.gameObject.SetActive(true);
+        }
+        //if NightVision currently active
+        else
+        {
+            MyVolume.profile = StandardProfile;
+            NightVisionActive = false;
+            NightVisionOverlay.gameObject.SetActive(false);
+        }
+    }
+
+}
