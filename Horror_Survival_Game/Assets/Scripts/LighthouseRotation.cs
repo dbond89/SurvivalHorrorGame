d1875318c043874403754e8d727bd9878478c36b
Derrick <derrickbond89@gmail.com>
2020-09-20 13:14:44 -0600

    Fixed FPSController errors, Added Lighthouse Light
    = FirstPersonController: added catch to make sure errors weren't thrown when no footstep audio files were linked to the controller
    + LighthouseRotation: created script that simply rotates the new lighthouse light gameobject in HorrorScene

---
+++ Horror_Survival_Game/Assets/Scripts/LighthouseRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Handles the rotation for the Lighthouse Light within HorrorScene
+ */
+public class LighthouseRotation : MonoBehaviour
+{
+    // For linking the Lighthouse Light gameobject
+    [SerializeField] GameObject LighthouseLightObject;
+    // The speed at which the lighthouse light will rotate
+    [SerializeField] float RotationSpeed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        LighthouseLightObject.transform.Rotate(0.0f, RotationSpeed, 0.0f, Space.World);
+    }
+}
